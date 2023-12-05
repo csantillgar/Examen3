9c17fa2be41518ae97dc4e018b4e0f316196bc4f
csantillgar <csantill@myuax.com>
2023-12-05 11:38:12 +0100

    Insertar Símbolos(prueba)

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -8,9 +8,15 @@ class Variant{
 class Environment{
 private:
     std::map<std::string,Variant> symbolTable;
+    Environment() {}
+public:
+    void insert(const std::string& name, const Variant& value){
+        symbolTable[name] = value;
+    }
 
 };
-
+Environment env;
+env.insert("x", 10);
 
 int main(){
 
